d7251efc0213f1e6400e054de6c14bfab3d09f5c
Jose Alejandro Avellaneda G <joseavellaneda88@gmail.com>
2018-10-26 06:24:45 -0500

    V6

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -32,7 +32,8 @@ double getTh (double thE)
 	return thR;
 }
 
-int getPntObs (int array[], int sizeA, int pnt)		//obtiene la celda correspondiente a un punto en el plano del ambiente
+//obtiene la celda correspondiente a un punto en el plano del ambiente
+int getPntObs (int array[], int sizeA, int pnt)		//(arreglo de x/y, tamaño de x/y, coordenada en x/y)
 {
     for (int i = 0; i < sizeA; i++)
     {
@@ -49,6 +50,8 @@ const int xMax = 8500;						 //tamaño del mapa en x
 const int yMax = 4000;						 //tamaño del mapa en y
 const int xSize = xMax/500;					 //numero de celdas en x
 const int ySize = yMax/500;					 //numero de celdas en y
+const int qi[2] = {600,3300};				 //coordenadas punto inicial
+const int qf[2] = {5100,300};				 //coordenadas punto final
 const int nObs = 2;							 //numero de obstaculos
 const int filas = nObs * 4;					 //puntos totales de obstaculos (2 x obs)
 int pntsObs[filas][2] = {{0,4200},{0,900},{5700,8000},{600,3900},{0,600},{900,2100},{5100,5700},{2700,3900}};  //matriz de puntos de los obs 
@@ -95,11 +98,24 @@ int ** getObs (int matrixOP[filas][2], int filasM, int arrayX[], int arrayY[], i
     return mCeldasObs;
 }
 
+int getCostMnht(int xcP1, int ycP1, int xcP2, int ycP2)
+{
+	int distMnht = abs(xcP1 - xcP2) + abs(ycP1 - ycP2);
+	return distMnht;
+}
+
 
 int main(int argc, char **argv)
 {
 	int* xCoorE = getAxe(xSize);			//calculo celdas del eje x
 	int* yCoorE = getAxe(ySize);			//calculo celdas del eje y
+	
+	// calculo de los qi y qf en la matriz
+	const int qixM = (getPntObs (xCoorE, xSize, qi[0])-250)/500;
+    const int qiyM = ySize-1-(getPntObs (yCoorE, ySize, qi[1])-250)/500;        //ajuste en y para visualizar en la matriz
+    
+    const int qfxM = (getPntObs (xCoorE, xSize, qf[0])-250)/500;
+    const int qfyM = ySize-1-(getPntObs (yCoorE, ySize, qf[1])-250)/500;        //ajuste en y para visualizar en la matriz
 
     int** mCObjetos = getObs(pntsObs,filas,xCoorE,yCoorE,xSize,ySize);			//matriz con puntos de obstaculos como celdas
   
@@ -111,10 +127,8 @@ int main(int argc, char **argv)
         
         for (int icS = 0; icS < filas; icS+= 2) // correr sobre los obstáculos
         {
-            cout << "menor " << mCObjetos[icS][0] << " mayor " << mCObjetos[icS][1] << endl;
             if (xCoorE[icO] >= mCObjetos[icS][0] && xCoorE[icO] <= mCObjetos[icS][1])
             {
-                cout << xCoorE[icO] << endl;
                 for (int icOy = 0; icOy < ySize;  icOy++)
                 {
                     if (yCoorE[icOy]  >= mCObjetos[icS+1][0] && yCoorE[icOy]  <= mCObjetos[icS+1][1])
@@ -127,14 +141,74 @@ int main(int argc, char **argv)
         
     }
             
-       
+    cSpace[qiyM][qixM] = 5;         //visaulizacion qi
+    cSpace[qfyM][qfxM] = 7;         //visaulizacion qf
+    
     for (int icS = 0; icS < ySize; icS++)
     {
       for (int jcS = 0; jcS < xSize; jcS++)
         {
-          cout << cSpace[icS][jcS] << "   ";
+          cout << cSpace[icS][jcS] << " ";
         }
         cout << endl;
     }
+    
+    //cout << getCostMnht(qixM,qiyM,qfxM,qfyM) << endl;
+    
+    //calculo del camino desde qi hasta qf
+    int xAdv = qixM;
+    int yAdv = qiyM;
+    const int nVecinos = 8;
+    int minD = 100;
+    bool posibleChoque = false;
+    int dist, bestX, bestY;
+    cout<< "qf (" << qfxM << ", " << qfyM << ")" << endl;
+    
+    while (xAdv != qfxM || yAdv != qfyM)
+    {
+        int xVecinos[nVecinos] = {xAdv, xAdv-1, xAdv, xAdv+1, xAdv+1, xAdv-1, xAdv-1, xAdv+1};
+        int yVecinos[nVecinos] = {yAdv+1, yAdv, yAdv-1, yAdv, yAdv+1, yAdv+1, yAdv-1, yAdv-1};
+        
+        for (int v = 0; v < nVecinos; v++)
+        {
+			if (xVecinos[v]>=0 && xVecinos[v]<xSize && yVecinos[v]>=0 && yVecinos[v]<ySize)
+            {
+                if (v>3)
+                {
+                    if (cSpace[yVecinos[v]][xAdv] == 1 || cSpace[yAdv][xVecinos[v]])
+                    {
+                        posibleChoque = true;
+                    }
+                }
+                
+                dist = getCostMnht(xVecinos[v], yVecinos[v], qfxM, qfyM);
+                //cout<< "vecino" << v << " (" << xVecinos[v] << ", " << yVecinos[v] << ") distancia " << dist << " celda " << cSpace[yVecinos[v]][xVecinos[v]] << endl;
+                if (minD > dist && cSpace[yVecinos[v]][xVecinos[v]]!=1 && posibleChoque == false)
+                {
+                    minD = dist;
+                    bestX = xVecinos[v];
+                    bestY = yVecinos[v];
+                }
+				posibleChoque = false;
+                
+            }
+        }
+        
+        xAdv = bestX;
+        yAdv = bestY;
+        cSpace[bestY][bestX] = 2;
+        //cout<< "(" << bestX << ", " << bestY << ")" << endl;
+        
+    } 
+    
+    for (int icS = 0; icS < ySize; icS++)
+    {
+      for (int jcS = 0; jcS < xSize; jcS++)
+        {
+          cout << cSpace[icS][jcS] << " ";
+        }
+        cout << endl;
+    }
+    
   return 0;
 }
